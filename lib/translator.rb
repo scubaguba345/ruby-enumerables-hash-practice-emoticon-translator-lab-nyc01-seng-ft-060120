4a2da0c09efd386aaf65d96bc193f5212672989e
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-17 23:57:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,21 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+emoticons = YAML.load_file(file_path)
+new_path = Hash.new
+emoticons.each do |key, meaning|
+  new_path[key] = Hash.new
+  new_path[key][:english] = meaning[0]
+  new_path[key][:japanese] = meaning[1]
+end
+new_path
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+
 end
 
 def get_english_meaning
-  # code goes here
+
 end
